537d30a7ff319c5ee8cd88c1d27b6eefc489ab64
unknown <matthewhknetsch@gmail.com>
2018-05-19 14:03:15 -0600

    blank

--- app/data/chars/dragonObjects.js
+++ app/data/chars/dragonObjects.js
@@ -16,3 +16,4 @@ var earthDragon = new Dragon("Derze the Mountain", 10, 15, 3, 120)
 var windDragon = new Dragon("Hera the Torrent", 10, 5, 15, 80)
 var lightningDragon = new Dragon("Strigga the Flash", 10, 10, 10, 100)
 var ultimaDragon = new Dragon("Tiamat the End", 20, 20, 20, 200)
+
--- app/logic/fightingLogic.js
+++ app/logic/fightingLogic.js
@@ -1,11 +1,27 @@
 //Handles how characters fight dragons
+var dragon = require("../data/chars/dragonObjects");
+var characters = require("../data/chars/characterObjects");
+var weapons = require("../data/equips/equipmentObjects");
 
+var damageTaken = attacker.str;
 // click for each party member action
-
+var attack = function(){
+    dodge();
+}
 // dragon hp = dragon hp - attackers str
 
 // option to defend which gives defense bonus
 
 // each attack has math.random to see if opponent dodges
+
+
+var dodge = function () {
+    var dodgeChance = (defender.agi % 10);
+    var hit = Math.random();
+    if (hit < dodgeChance) {
+        damageTaken = 0;
+    };
+    defender.hp = defender.hp - damageTaken;
+};
 
 // if statement to check that party members hp is above 0 to allow them to attack
