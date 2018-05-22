47964a37d9a60c6c0891e9eced015d108afe6a40
Dylan-Desktop <dylanmthomas@live.com>
2018-05-21 21:27:56 -0600

    adjusted stats and deleted faulty characterLogic theory

--- app/data/chars/characterObjects.js
+++ app/data/chars/characterObjects.js
@@ -1,7 +1,7 @@
 const equipment = require('../equips/equipmentObjects.js')
 
 //Objects of different Characters to choose from
-var Class = function(name, str, def, agi, mag, hp, weapons){
+var Class = function(name, str, def, agi, mag, hp){
     this.name = name,
     this.str = str,
     this.def = def,
@@ -12,11 +12,11 @@ var Class = function(name, str, def, agi, mag, hp, weapons){
     this.armor = [equipment.helmet, equipment.chestPlate, equipment.greaves, equipment.boots, equipment.gloves]
 }
 
-var fighter = new Class("Fighter", 5, 10, 2, 0, 20)
+var fighter = new Class("Fighter", 5, 5, 2, 0, 30)
 fighter.weapon.push(equipment.sword, equipment.shield)
-var mage = new Class("Mage", 2, 2, 2, 10, 15)
+var mage = new Class("Mage", 2, 2, 2, 10, 25)
 mage.weapon.push(equipment.staff)
-var rogue = new Class("Rogue", 2, 2, 10, 0, 15)
+var rogue = new Class("Rogue", 2, 2, 10, 0, 25)
 rogue.weapon.push(equipment.dagger)
 rogue.sneakAttack = ()=>{
     var critChance = Math.floor(Math.random() * Math.floor(4))
@@ -28,7 +28,7 @@ rogue.sneakAttack = ()=>{
     }
 }
 
-var healer = new Class("Healer", 0, 2, 2, 10, 10)
+var healer = new Class("Healer", 0, 2, 2, 10, 20)
 healer.weapon.push(equipment.wand, equipment.shield)
 
 module.exports = {
